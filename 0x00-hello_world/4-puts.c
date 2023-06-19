588a0b960d9474f10b9aa43757ed55c6a78378fd
ahmed <a.mostafahasan.com>
2023-06-19 12:18:50 -0700

    done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,15 @@
-#inc lude <stdio.h>
-int main(void){
+#include <stdio.h>
+
+/**
+ * main - Entry point 
+ *
+ * Description: A C program that prints with put function 
+ *
+ * return: Always 0 (success)
+*/
+
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }	
