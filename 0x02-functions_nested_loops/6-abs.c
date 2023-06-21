86743318aa48c828b8f39b61365a830020986e0d
ahmed <a.mostafahasan.com>
2023-06-21 09:50:00 -0700

    done

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - function that cpmputes the absolute
+ * 		value of an integer
+ *
+ * 		@n: takes in integer type input for function
+ *
+ * Rutern: Always 0 (Success)
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		n = (-1) * n;
+	return
